8b7f121c4adabb7be10dcbd492c2e35e1aaabc05
Marco Casali <marco.casali13@studio.unibo.it>
2024-05-14 00:32:18 +0200

    started gamemap factory implementation

---
+++ src/main/java/it/unibo/model/map/GameMapFactoryImpl.java
@@ -0,0 +1,112 @@
+package it.unibo.model.map;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+import org.json.JSONObject;
+import it.unibo.model.map.tile.Tile;
+import it.unibo.model.map.tile.TileFeature;
+import it.unibo.model.utilities.Position2D;
+import it.unibo.model.utilities.Vector2D;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+
+public class GameMapFactoryImpl implements GameMapFactory {
+    private static final String JSON_EXTENSION = ".json";
+    private static final String MAP_RESOURCES = "maps/";
+    private static final String JSON_ROWS_KEY = "rows";
+    private static final String JSON_COLUMNS_KEY = "columns";
+    private static final String JSON_TILES_KEY = "tiles";
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public GameMap fromJSON(String source) {
+        final JSONObject json = new JSONObject(source);
+        final Map<Position2D,Tile> tiles = new HashMap<>();
+
+        //rows
+        final int rows = json.getInt(JSON_ROWS_KEY);
+        //columns
+        final int columns = json.getInt(JSON_COLUMNS_KEY);
+        //tiles
+
+
+        return generic(rows,columns, tiles);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public GameMap fromJSONFile(String fileName) throws IOException {
+        final Path path = Path.of(ClassLoader.getSystemResource(MAP_RESOURCES + fileName).toString());
+        return fromJSON(new String(Files.readAllBytes(path)));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public GameMap fromName(String name) throws IOException {
+        return fromJSONFile(name + JSON_EXTENSION);
+    }
+
+    private GameMap generic(final int nRows, final int nColumns, final Map<Position2D, Tile> tilesMap) {
+        return new GameMap() {
+            private final Map<Position2D, Tile> tiles = tilesMap;
+            private final int rows = nRows;
+            private final int columns = nColumns;
+            
+            @Override
+            public int getRows() {
+                return this.rows;
+            }
+
+            @Override
+            public int getColumns() {
+                return this.columns;
+            }
+
+            @Override
+            public Stream<Tile> getTiles() {
+                return this.tiles.values().stream();
+            }
+
+            @Override
+            public Stream<Tile> getDefenseTiles() {
+                return getTiles().filter(Tile::canBuild);
+            }
+
+            @Override
+            public Position2D getSpawnPosition() {
+                return this.tiles.entrySet().stream()
+                    .filter(entry -> entry.getValue().getTileFeatures()
+                    .contains(TileFeature.PATH_START)).findFirst().get().getKey();
+            }
+
+            @Override
+            public Vector2D getPathDirection(final Position2D position) {
+                final Set<TileFeature> directions = this.tiles.get(edge(position)).getTileFeatures();
+                if (directions.contains(TileFeature.MOVE_DOWN)) {
+                    return new Vector2D(0, -1);
+                } else if (directions.contains(TileFeature.MOVE_UP)) {
+                    return new Vector2D(0, 1);
+                } else if (directions.contains(TileFeature.MOVE_RIGHT)) {
+                    return new Vector2D(1, 0);
+                } else if (directions.contains(TileFeature.MOVE_LEFT)) {
+                    return new Vector2D(-1, 0);
+                } else {
+                    throw new IllegalStateException();
+                }
+            }
+
+            private Position2D edge(final Position2D position) {
+                return new Position2D((int) (position.x() * this.columns), (int) (position.y() * this.rows));
+            }
+        };
+    }
+}
