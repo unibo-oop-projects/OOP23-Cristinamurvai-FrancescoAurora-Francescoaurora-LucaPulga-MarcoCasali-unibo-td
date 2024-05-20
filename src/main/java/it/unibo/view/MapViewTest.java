3910f8b0230f35fdb692dac5ac3b24e509811bc6
Francesco Aurora <francesco.aurora@studio.unibo.it>
2024-05-20 20:40:53 +0200

    window resize in mapviewtest

--- src/main/java/it/unibo/view/MapViewTest.java
+++ src/main/java/it/unibo/view/MapViewTest.java
@@ -4,18 +4,23 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+
 import it.unibo.model.map.GameMap;
 import it.unibo.model.map.GameMapFactory;
 import it.unibo.model.map.GameMapFactoryImpl;
 import it.unibo.model.map.tile.Tile;
-import java.awt.GridLayout;
 
 public class MapViewTest extends JFrame {
     private JPanel contentPanel = new JPanel();
     private final GameMapFactory factory = new GameMapFactoryImpl();
+    private GameMap map;
 
     public MapViewTest() {
-        GameMap map = null;
+        map = null;
         try {
             map = factory.fromName("test");
         } catch (Exception e) {
@@ -30,11 +35,32 @@ public class MapViewTest extends JFrame {
         this.pack();
         this.revalidate();
         this.setVisible(true);
+
+        this.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                resizeImages();
+            }
+        });
     }
 
     private void addTile(Tile t) {
         final JLabel cell = new JLabel();
         cell.setIcon(new ImageIcon(ClassLoader.getSystemResource(t.getSprite())));
         this.contentPanel.add(cell);
+    }
+
+    private void resizeImages() {
+        int cellWidth = contentPanel.getWidth() / map.getColumns();
+        int cellHeight = contentPanel.getHeight() / map.getRows();
+
+        for (int i = 0; i < contentPanel.getComponentCount(); i++) {
+            JLabel label = (JLabel) contentPanel.getComponent(i);
+            ImageIcon icon = (ImageIcon) label.getIcon();
+            Image img = icon.getImage().getScaledInstance(cellWidth, cellHeight, Image.SCALE_SMOOTH);
+            label.setIcon(new ImageIcon(img));
+        }
+
+        contentPanel.revalidate();
     }
 }
