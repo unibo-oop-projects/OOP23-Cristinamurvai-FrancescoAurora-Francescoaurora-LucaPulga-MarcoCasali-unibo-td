a857f4a26f8b9941625103e1307914d308f3f28e
francescoaurora <francesco.aurora@studio.unibo.it>
2024-06-28 17:02:10 +0200

    Fix style DistanceBasedTargetSelection.java

--- src/main/java/it/unibo/model/entities/defense/tower/target/DistanceBasedTargetSelection.java
+++ src/main/java/it/unibo/model/entities/defense/tower/target/DistanceBasedTargetSelection.java
@@ -8,15 +8,17 @@ import it.unibo.model.entities.enemies.Enemy;
 import it.unibo.model.utilities.Position2D;
 
 /**
- * Strategy pattern for choosing the type of the tower's target method. This one is based on the distance between the towers and the enemies.
+ * Strategy pattern for choosing the type of the tower's target method. This one
+ * is based on the distance between the towers and the enemies.
  */
 public class DistanceBasedTargetSelection implements TargetSelectionStrategy {
 
     /**
-     * {@link Tower}'s target method to target {@link Enemy}.
-     * Attacking @param tower.
-     * All the @param enemies alive.
-     * @return {@code Optional} {@link Enemy} chosen based on the {@link Tower}'s' target strategy. 
+     * {@link Tower}'s target method to target {@link Enemy}. Attacking @param
+     * tower All the @param enemies alive.
+     *
+     * @return {@code Optional} {@link Enemy} chosen based on the
+     * {@link Tower}'s' target strategy.
      */
     @Override
     public Optional<Enemy> selectTarget(final Tower tower, final Set<Enemy> enemies) {
