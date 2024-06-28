4060ffd51bc536eaebaeda7e699a77a1cf5e08f7
francescoaurora <francesco.aurora@studio.unibo.it>
2024-06-28 16:59:29 +0200

    Fix style SingleTargetAttack.java

--- src/main/java/it/unibo/model/entities/defense/tower/attack/SingleTargetAttack.java
+++ src/main/java/it/unibo/model/entities/defense/tower/attack/SingleTargetAttack.java
@@ -14,14 +14,15 @@ import it.unibo.model.utilities.Vector2D;
  */
 public class SingleTargetAttack implements AttackStrategy {
 
-    private final int SLOW_FREQ = 5000;
-    private final double BULLET_SPEED = 0.15;
-    private final int BULLET_DAMAGE = 5000;
+    private static final int SLOW_FREQ = 5000;
+    private static final double BULLET_SPEED = 0.15;
+    private static final int BULLET_DAMAGE = 5000;
 
     /**
-     * {@link Tower}'s attack method to attack target {@link Enemy}.
-     * Attacking @param tower.
-     * Target @param enemy chosen by the {@link Tower} depending on the {@link TargetSelectionStrategy}.
+     * {@link Tower}'s attack method to attack target {@link Enemy}. Attacking
+     *
+     * @param tower Target @param enemy chosen by the {@link Tower} depending on
+     * the {@link TargetSelectionStrategy}.
      */
     @Override
     public void attack(final Tower tower, final Optional<Enemy> enemy) {
@@ -34,7 +35,8 @@ public class SingleTargetAttack implements AttackStrategy {
             if (timeSinceLastShot >= fireRateInMilliseconds) {
                 System.out.println(tower.getName() + " attacco a " + e.getName());
                 Vector2D direction = Vector2D.calculateDirection(tower.getPosition(), e.getPosition());
-                Bullet bullet = new BulletImpl(1, "bullet", "base", "bullet/img/bullet.png", tower.getPosition(), direction, BULLET_SPEED, BULLET_DAMAGE, e);
+                Bullet bullet = new BulletImpl(1, "bullet", "base", "bullet/img/bullet.png", tower.getPosition(),
+                        direction, BULLET_SPEED, BULLET_DAMAGE, e);
 
                 tower.getBullets().add(bullet);
                 tower.getCurrentWeapon().setLastShotTime(currentTime);
