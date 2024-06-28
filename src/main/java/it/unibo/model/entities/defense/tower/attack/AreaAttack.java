9fcb95e7fc5ed8036209f331c38da1bd11f41fcd
francescoaurora <francesco.aurora@studio.unibo.it>
2024-06-28 16:51:59 +0200

    Fix style AreaAttack.java

--- src/main/java/it/unibo/model/entities/defense/tower/attack/AreaAttack.java
+++ src/main/java/it/unibo/model/entities/defense/tower/attack/AreaAttack.java
@@ -7,17 +7,20 @@ import it.unibo.model.entities.defense.tower.target.TargetSelectionStrategy;
 import it.unibo.model.entities.enemies.Enemy;
 
 /**
- * Strategy pattern implmentation. Attack based on an area target, multiple attack.
+ * Strategy pattern implmentation. Attack based on an area target, multiple
+ * attack.
  */
 public class AreaAttack implements AttackStrategy {
 
     /**
-     * {@link Tower}'s attack method to attack target {@link Enemy}.
-     * Attacking @param tower.
-     * Target @param enemy chosen by the {@link Tower} depending on the {@link TargetSelectionStrategy}.
+     * {@link Tower}'s attack method to attack target {@link Enemy}. Attacking
+     *
+     * @param tower. Target @param enemy chosen by the {@link Tower} depending
+     * on the {@link TargetSelectionStrategy}.
      */
     @Override
     public void attack(final Tower tower, final Optional<Enemy> enemy) {
-        throw new UnsupportedOperationException("Attack based on an area target, multiple attack, will be implemented in a new release version of UNIBO-TD.");
+        throw new UnsupportedOperationException(
+                "Attack based on an area target, multiple attack, will be implemented in a new release version of UNIBO-TD.");
     }
 }
